1afb32610c9582c28e35b01db0e9a83233ca53ec
Tyler Mestery <tyler@mestery.com>
2025-05-09 19:50:19 -0500

    Adding more to cleaning and tokenizing text.

--- Main.py
+++ Main.py
@@ -1,4 +1,3 @@
-# importing necessary packages
 import nltk
 nltk.download('punkt_tab')
 import spacy
@@ -6,18 +5,18 @@ from collections import Counter
 
 # Main class
 def main():
-    # Download the required NLTK resource + oad the spaCy model
+    # Download the required NLTK resource + Add the spaCy model
     nltk.download('punkt')
     nlp = spacy.load('en_core_web_sm')  
 
     # Read text into a string variable
-    textString = read_text_into_string("MobyDickBook.txt")
+    textString = read_text_and_clean("MobyDickBook.txt")
 
     # Run the function to clean/tokenize .txt file
-    clean_and_tokenize(textString)
+    tokenize_the_string(textString)
 
 # Get the sample text into string format
-def read_text_into_string(file_path):
+def read_text_and_clean(file_path):
     with open(file_path, 'r') as file:
         file_content = ""
         line = file.readline()
@@ -26,15 +25,22 @@ def read_text_into_string(file_path):
             file_content += line
             line = file.readline()
 
-    return file_content
+    # Make it all lowercase text
+    cleanedText = file_content.lower()
+
+    # Remove all punctuation from text
+    for punctuation in ',.?;"-':
+        cleanedText = text.replace(punctuation, "")
+
+    return cleanedText
 
 # Takes a text file, cleans it, tokenizes it, and saves cleaned word list for later use
-def clean_and_tokenize(stringName):
+def tokenize_the_string(stringName):
     # Tokenize the text and create bigrams
     words = nltk.word_tokenize(stringName.lower())
     bigrams = zip(words, words[1:])
     counts = Counter(bigrams)
 
-    print(counts)
+    # print(counts)
 
-    #return counts
+    # return counts
