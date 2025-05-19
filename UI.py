1be21f0afe17df19fa12e21969e846438b06980a
Tyler Mestery <tyler@mestery.com>
2025-05-18 21:18:34 -0500

    Fixed import statement on UI

--- UI.py
+++ UI.py
@@ -1,6 +1,6 @@
 # Usual imports along with downloading punkt again...
 import streamlit as st
-from markov_model import read_text_and_clean, build_markov_model, generate_text
+import Main
 import nltk
 nltk.download('punkt')
 
