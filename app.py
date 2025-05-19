5502664e05ebb8e4558ca36847de2194105d6c6e
Tyler Mestery <tyler@mestery.com>
2025-05-19 11:27:59 -0500

    Adding different app functionalities

--- app.py
+++ app.py
@@ -2,6 +2,10 @@
 import streamlit as st
 from Main import read_text_and_clean, build_markov_model, generate_text, tokenize_text
 
+# Adding logo (thanks, for now, grok)
+main_logo = "logo.jpg"
+st.logo(main_logo)
+
 # Read and process text
 string_text = read_text_and_clean("samples/MobyDickBook.txt")
 token_text = tokenize_text(string_text)
@@ -16,3 +20,9 @@ input_length = st.number_input("How many words to generate?", min_value=1, step=
 if st.button("Generate"):
     output = generate_text(model, input_word.lower(), int(input_length))
     st.write(output)
+
+# Feedback button
+star_map = ["1", "2", "3", "4", "5"]
+selected_star = st.feedback("stars")
+if selected is not None:
+    st.markdown(f"You selected {star_map[selected_star]} star(s)!")
